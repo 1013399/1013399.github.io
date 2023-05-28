da6936b3bee68af885dcd5fe70d92513052127aa
Eva <im1010ioio@gmail.com>
2023-05-29 02:55:44 +0800

    use theme-hope

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,20 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+  lang: "zh-Hant-tw",
+
+  locales: {
+    "/": {
+      lang: "zh-TW",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
