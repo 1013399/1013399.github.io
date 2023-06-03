47dc33ac9fe13786178a2344e4545dd86dff6e9e
陳依琳 <im1010ioio@gmail.com>
2023-06-03 12:51:39 +0800

    加入 Waline 留言板功能

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -94,8 +94,8 @@ export default hopeTheme({
     blog: true,
 
     comment: {
-      // @ts-expect-error: You should generate and use your own comment service
-      provider: "Giscus",
+      provider: "Waline",
+      serverURL: "https://1013399-waline.vercel.app/", // your server url
     },
 
     // all features are enabled for demo, only preserve features you need here
