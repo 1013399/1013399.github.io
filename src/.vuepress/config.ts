c5274dcd2f991eec7b8b3d01378ed3f11bd4c73a
陳依琳 <im1010ioio@gmail.com>
2023-07-19 00:16:58 +0800

    add sitemapPlugin

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { searchPlugin } from '@vuepress/plugin-search'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { sitemapPlugin } from "vuepress-plugin-sitemap2";
 
 export default defineUserConfig({
   base: "/",
@@ -25,6 +26,9 @@ export default defineUserConfig({
     }),
     googleAnalyticsPlugin({
       id: 'G-27XJ94VCNL',
+    }),
+    sitemapPlugin({
+      hostname: 'https://1013399.github.io/'
     }),
   ],
 
